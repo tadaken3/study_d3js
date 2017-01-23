bf86545af1df9285c4964559c9f1dd14ebbba986
tadaken3 <k.tanaka6057@gmail.com>
2017-01-23 23:27:21 +0900

    treemapにテキストを追加

--- js/treemap.js
+++ js/treemap.js
@@ -1,22 +1,26 @@
+var color10 = d3.scale.category10()
+
 d3.csv("./data_.csv", function(error, data){
     var tmp = [ ];
     for(var i=0; i<data.length; i++){
-        tmp.push({name : data[i]["app_name"], value : data[i]["grossing"]});
+        tmp.push({name : data[i]["app_name"],
+        value : data[i]["grossing"],
+        category : data[i]["category"]});
     }
      var dataset = {
          children: tmp
     };
-    console.log(dataset);
     drawTreemap(dataset);
 })
 
 function drawTreemap(dataset){
     var treemap = d3.layout.treemap()
         .size([800, 800])
-
+        
     var tmap = d3.select("#treemap")
             .selectAll("rect")
             .data(treemap.nodes(dataset))
+
         tmap.enter()
             .append("rect")
             .attr("class", "block")
@@ -29,22 +33,20 @@ function drawTreemap(dataset){
             .attr("width", function(d, i){
                 return d.dx;
             })
-            .attr("height", function(d ,i){
+            .attr("height", function(d, i){
                 return d.dy;
-            })
-            
+            });
         tmap.enter()
             .append("text")
             .attr("class", "name")
-            .attr("transform", function(d, i){
-                return "translate(" + (d.x+d.dx/2) + "," + (d.y+d.dy/2) + ")";
+            .attr("transform", function(d,i){
+                return "translate(" + (d.x + d.dx/2) + "," + (d.y + d.dy/2) + ")";
             })
-            .atter("dy", "0.2em")
-            .text(function(d, i) {
+            .attr("dy", "0.2em")
+            .text(function(d,i){
                 return d.name;
             })
-            .attr("stroke", "black")
             .style("font-size", function(d, i){
-                return (d.dx * d.dy) /200;  
+                return (d.dx + d.dy) / 20 + "px";
             })
 }
